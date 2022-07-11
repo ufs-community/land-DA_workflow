4ca68aba149f44372abff1929c5077a7b9c6df27
Zhichang Guo <Zhichang.Guo@noaa.gov>
2022-07-11 17:21:28 +0000

    resolve issues found in Clara's review

--- ensemble_pert/lndp_apply_pert.f90
+++ ensemble_pert/lndp_apply_pert.f90
@@ -21,12 +21,13 @@ program lndp_apply_pert
 
   type namelist_type
     character*256      :: namelist_name = ""             ! namelist file name
-    character*256      :: lndp_input_file = ""           ! input file for perturbation
-    character*256      :: var_input_file = ""            ! input file for vegetation fraction
-    character*256      :: output_file = ""               ! output file name
-    character(len=128) :: lndp_var_list(max_n_var_lndp)  ! variable names in the perturbation file
-    real               :: lndp_prt_list(max_n_var_lndp)  ! adjustment factor for each perturbation
-    integer            :: n_var_lndp                     ! actual numbers of perturbation variables
+    character*256      :: stochy_input_file = ""         ! file with perturbation pattern
+    character*256      :: var_input_file = ""            ! file with data that will be perturbed
+    character*256      :: output_file = ""               ! output file with perturbed data
+    character(len=128) :: lndp_var_list(max_n_var_lndp)  ! list of variables to be perturbed". 
+                                                         ! current options: vgf (vegetation fraction)
+    real               :: lndp_prt_list(max_n_var_lndp)  ! list of perturbation magnitudes
+    integer            :: n_var_lndp                     ! number of variables being perturbed
   end type namelist_type
 
   type(namelist_type) :: namelist
@@ -56,7 +57,7 @@ program lndp_apply_pert
   call ReadNamelist(namelist)
 
   ! read in vector length from the perturbation file
-  call ReadVectorLength(namelist%lndp_input_file, vector_length)
+  call ReadVectorLength(namelist%stochy_input_file, vector_length)
 
   ! read in vector length from the variable file and do sanity check
   call ReadVectorLength(namelist%var_input_file, vector_length)
@@ -92,7 +93,7 @@ program lndp_apply_pert
          allocate(vfrac(vector_length))
 
          ! read in perturbation pattern
-         call ReadVectorVariable(namelist%lndp_input_file, namelist%lndp_var_list(v), vector_length, 1, sfc_wts)
+         call ReadVectorVariable(namelist%stochy_input_file, namelist%lndp_var_list(v), vector_length, 1, sfc_wts)
 
          ! open the output file
          status = nf90_open(namelist%output_file, nf90_write, ncid)
@@ -134,7 +135,7 @@ contains
 
     integer, parameter  :: max_n_var_lndp = 20
     type(namelist_type) :: namelist
-    character*256       :: lndp_input_file
+    character*256       :: stochy_input_file
     character*256       :: var_input_file
     character*256       :: output_file
     character(len=128)  :: lndp_var_list(max_n_var_lndp)
@@ -142,7 +143,7 @@ contains
     integer             :: n_var_lndp
     integer             :: k
 
-    namelist / run_setup  / lndp_input_file, var_input_file, output_file, lndp_var_list, lndp_prt_list, n_var_lndp
+    namelist / run_setup  / stochy_input_file, var_input_file, output_file, lndp_var_list, lndp_prt_list, n_var_lndp
 
     lndp_var_list = 'XXX'
     lndp_prt_list = -999.
@@ -151,9 +152,9 @@ contains
      read(30, run_setup)
     close(30)
 
-    namelist%lndp_input_file  = lndp_input_file
-    namelist%var_input_file  = var_input_file
-    namelist%output_file = output_file
+    namelist%stochy_input_file = stochy_input_file
+    namelist%var_input_file    = var_input_file
+    namelist%output_file       = output_file
     n_var_lndp= 0
     do k =1,size(lndp_var_list)
        if ((trim(lndp_var_list(k)) .EQ. 'XXX') .or. (lndp_prt_list(k) .LE. 0.)) then
