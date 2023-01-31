3857ba76c2cf19b441e693cb7a4bdec53f6200dc
gspetro <gillian.s.petro@gmail.com>
2023-01-31 14:46:08 -0500

    add formatting, bib, other settings to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,74 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-#project = 'UFS Offline LandDA'
-#copyright = ''
-#author = ''
-#release = '0.0'
+project = 'UFS Offline Land DA'
+copyright = '2023'
+author = ' '
+
+# The short X.Y version
+version = 'develop'
+# The full version, including alpha/beta/rc tags
+release = 'Develop Branch Documentation'
+
+numfig = True
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme',
+    #'sphinx.ext.autodoc',
+    #'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    #'sphinx.ext.todo',
+    #'sphinx.ext.coverage',
+    #'sphinx.ext.mathjax',
+    #'sphinx.ext.ifconfig',
+    #'sphinx.ext.viewcode',
+    #'sphinx.ext.githubpages',
+    #'sphinx.ext.napoleon',
+    'sphinxcontrib.bibtex',
+]
 
+# File with bibliographic info
+bibtex_bibfiles = ['references.bib']
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+# html_theme_options = {}
+html_theme_options = {"body_max_width": "none"}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {}
+
+def setup(app):
+    app.add_css_file('custom.css')  # may also be an URL
+    app.add_css_file('theme_overrides.css')  # may also be a URL
