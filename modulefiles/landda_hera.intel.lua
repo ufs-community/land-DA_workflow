5dca799780f799235bb507bdedee505790aa6e1d
Mark A Potts <Mark.Potts@noaa.gov>
2023-02-10 18:20:55 +0000

    change module files to lua and put them in modulefiles

---
+++ modulefiles/landda_hera.intel.lua
@@ -0,0 +1,27 @@
+help([[
+loads Land DA prerequisites for Hera/Intel
+]])
+
+setenv("EPICHOME", "/scratch1/NCEPDEV/nems/role.epic")
+
+prepend_path("MODULEPATH", pathJoin(os.getenv("EPICHOME"),"miniconda3/modulefiles"))
+
+miniconda3_ver=os.getenv("miniconda3_ver") or "4.12.0"
+load(pathJoin("miniconda3", miniconda3_ver))
+
+prepend_path("MODULEPATH", pathJoin(os.getenv("EPICHOME"),"spack-stack/envs/landda-release-1.0-intel/install/modulefiles/Core"))
+
+load("stack-intel")
+load("stack-intel-oneapi-mpi")
+load("netcdf-c")
+load("netcdf-fortran")
+load("cmake")
+load("ecbuild")
+load("stack-python")
+
+setenv("CC", "mpiicc")
+setenv("CXX", "mpiicpc")
+setenv("FC", "mpiifort")
+setenv("JEDI_INSTALL", pathJoin(os.getenv("EPICHOME"),"contrib"))
+
+whatis("Description: Land DA build environment")
