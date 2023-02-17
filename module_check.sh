f45c7ed2f7803845680bc814c4098783cfa409e4
ulmononian <cameron_book@alumni.brown.edu>
2023-02-17 14:58:43 -0600

    Update module_check.sh due to envar changes.

--- module_check.sh
+++ module_check.sh
@@ -11,18 +11,23 @@ elif [[ ${USE_SINGULARITY} =~ yes ]]; then
   MACHINE=singularity
 fi
 
-# try to load the modules
-module use ${LAND_OFFLINE_WORKFLOW}/modulefiles
-module load landda_${MACHINE}.intel
-
 # check which modules are required and notify the user if they are not currently loaded in the environment.
 
-env_mods=($(grep -o 'load("[^"]*")' ${LAND_OFFLINE_WORKFLOW}/modulefiles/landda_${MACHINE}.intel.lua | sed 's/load("//;s/")//'))
+env_mods=($(grep -o 'load("[^"]*")' ${CYCLEDIR}/modulefiles/landda_${MACHINE}.intel.lua | sed 's/load("//;s/")//'))
+
+missing_mods=()
 
 for mod in ${env_mods[@]}
 do
   if ! module is-loaded "${mod}"; then
-     echo "${mod} is not loaded in the current environment. Please load ${mod} and re-submit $0."
+     missing_mods+=("${mod}")
   fi
 done
 
+if [[ ${#missing_mods[@]} -gt 0 ]]; then
+  echo "Error: the following modules are not loaded in the current environment: ${missing_mods[@]}. Please load them via 'module use land-offline_workflow/modulefiles; module load landda_${MACHINE}.intel' and then re-launch do_submit_cycle.sh."
+  exit 1
+else 
+ echo "All modules properly loaded in environment. Continuing!"
+ exit 0
+fi
