19e5e43454829f749b34883ac5fe9074d62531d9
rhaesung <rhaesung.kim@noaa.gov>
2023-08-15 22:29:49 +0000

    update and correct the GHCN obs timing

--- test/test_letkfoi_snowda.sh
+++ test/test_letkfoi_snowda.sh
@@ -10,7 +10,8 @@ source ${project_source_dir}/test/runtime_vars.sh ${project_binary_dir} ${projec
 
 # set extra paths
 OROG_PATH=$TPATH
-OBSDIR=${LANDDA_INPUTS}/DA/snow_depth
+#OBSDIR=${LANDDA_INPUTS}/DA/snow_depth
+OBSDIR=${OBSDIR:-"/scratch2/NCEPDEV/land/data/DA/"}
 
 # set executables
 JEDI_EXEC=${JEDI_EXEC:-$JEDI_EXECDIR/fv3jedi_letkf.x}
@@ -28,16 +29,23 @@ do
 done
 
 # prepare yaml files
-cp $project_source_dir/DA_update/jedi/fv3-jedi/yaml_files/release-v1.0/${DAtype}.yaml letkf_land.yaml
+cp $project_source_dir/DA_update/jedi/fv3-jedi/yaml_files/${fv3bundle_vn}/${DAtype}.yaml letkf_land.yaml
 for ii in "${!OBS_TYPES[@]}";
 do
     echo "============================= ${OBS_TYPES[$ii]}" 
-    cat $project_source_dir/DA_update/jedi/fv3-jedi/yaml_files/release-v1.0/${OBS_TYPES[$ii]}.yaml >> letkf_land.yaml
+    cat $project_source_dir/DA_update/jedi/fv3-jedi/yaml_files/${fv3bundle_vn}/${OBS_TYPES[$ii]}.yaml >> letkf_land.yaml
 
     # link ioda obs file
-    [[ -e ${OBS_TYPES[$ii]}_${YY}${MM}${DD}${HH}.nc ]] && rm ${OBS_TYPES[$ii]}_${YY}${MM}${DD}${HH}.nc
-    src_file=${OBSDIR}/${OBS_TYPES[$ii]}/data_proc/${YY}/${OBS_TYPES[$ii],,}_snwd_ioda_${YY}${MM}${DD}.nc
-    ln -fs ${src_file} ./${OBS_TYPES[$ii]}_${YY}${MM}${DD}${HH}.nc 
+    # GHCN are time-stamped at 18. If assimilating at 00, need to use previous day's obs, so that
+    # obs are within DA window.
+    [[ -e ${OBS_TYPES[$ii]}_${YY}${MP}${DP}${HP}.nc ]] && rm ${OBS_TYPES[$ii]}_${YY}${MP}${DP}${HP}.nc
+    obs_file=${OBSDIR}/snow_depth/${OBS_TYPES[$ii]}/data_proc/v3/${YY}/${OBS_TYPES[$ii],,}_snwd_ioda_${YY}${MP}${DP}.nc
+    if [[ -e $obs_file ]]; then
+      echo "${OBS_TYPES[$ii]} observations found: $obs_file"
+    else
+      echo "${OBS_TYPES[$ii]} observations not found: $obs_file"
+    fi
+    ln -fs $obs_file ./${OBS_TYPES[$ii]}_${YY}${MM}${DD}${HH}.nc 
 
 done
 
@@ -49,7 +57,9 @@ sed -i -e "s/XXYYYP/${YP}/g" letkf_land.yaml
 sed -i -e "s/XXMP/${MP}/g" letkf_land.yaml
 sed -i -e "s/XXDP/${DP}/g" letkf_land.yaml
 sed -i -e "s/XXHP/${HP}/g" letkf_land.yaml
-sed -i -e "s#DATAPATH#${OROG_PATH}#g" letkf_land.yaml
+sed -i -e "s/XXTSTUB/${TSTUB}/g" letkf_land.yaml
+sed -i -e "s#XXTPATH#${TPATH}#g" letkf_land.yaml
+#sed -i -e "s#DATAPATH#${OROG_PATH}#g" letkf_land.yaml
 sed -i -e "s/XXRES/${RES}/g" letkf_land.yaml
 RESP1=$((RES+1))
 sed -i -e "s/XXREP/${RESP1}/g" letkf_land.yaml
@@ -63,4 +73,5 @@ ln -fs $JEDI_STATICDIR ./
 
 #
 echo "============================= calling ${JEDI_EXEC}"
-${MPIRUN} -n $NPROC ${JEDI_EXEC} letkf_land.yaml
+srun -n $NPROC ${JEDI_EXEC} letkf_land.yaml
+#${MPIRUN} -n $NPROC ${JEDI_EXEC} letkf_land.yaml
