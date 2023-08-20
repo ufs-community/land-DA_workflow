e200879cb304e75043695019d3edc098a919ec53
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-08-20 17:38:00 -0400

    Update and rename job_card to hera_ctest.sh

---
+++ hera_ctest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -eux
+echo $USER
+
+ln -fs /scratch2/NAGAPE/epic/UFS_Land-DA/inputs /scratch1/NCEPDEV/stmp2/role.epic/jenkins/workspace/ && module use modulefiles && module load landda_hera.intel && mkdir build && cd build && ecbuild ../ && make -j4 && ctest
