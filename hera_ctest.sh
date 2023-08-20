c07ad9c37f67bc93312e0523f7775cc887df5f16
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-08-20 18:29:48 -0400

    Update hera_ctest.sh

--- hera_ctest.sh
+++ hera_ctest.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=offline_noahmp
-#SBATCH --account=nems
-#SBATCH --qos=debug
 set -eux
+
 echo $USER
 
 ln -fs /scratch2/NAGAPE/epic/UFS_Land-DA/inputs /scratch1/NCEPDEV/stmp2/role.epic/jenkins/workspace/ && module use modu\
