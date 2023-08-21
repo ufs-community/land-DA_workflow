9cfc7c238fe2a0873dc390a2c5236b2dd335fb16
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-08-20 22:00:50 -0400

    Update hera_ctest.sh

--- hera_ctest.sh
+++ hera_ctest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
 
-echo $USER
 
 ln -fs /scratch2/NAGAPE/epic/UFS_Land-DA/inputs /scratch1/NCEPDEV/stmp2/role.epic/jenkins/workspace/
 module use ../modulefiles && module load landda_hera.intel
