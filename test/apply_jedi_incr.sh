8c0f8de79ed1f0d6faa3bf6393af4ea548f92313
Rhae Sung Kim <rhaesung.kim@noaa.gov>
2023-09-01 16:25:39 -0400

    Update to use the JEDI Skylab v4 (#28)
    * update pathsn for EPICHOME and JEDI_INSTALL
    
    * update JEDI EXEC
    
    * replace srun with mpiexec
    
    * correct the TEST_BASEDIR path and clean up
    
    * update s3 bucket location
    
    * disable git action
    
    * Rename auto_doc._yml to auto_doc.yml

--- test/apply_jedi_incr.sh
+++ test/apply_jedi_incr.sh
@@ -46,7 +46,7 @@ done
 
 echo "============================= calling apply snow increment"
 #
-$MPIRUN -n $NPROC ${EXECDIR}/${TEST_EXEC}
+${MPIRUN} -n $NPROC ${EXECDIR}/${TEST_EXEC}
 
 # move ana tile to ./restarts/ana/tile
 mkdir -p ana/restarts/tile
