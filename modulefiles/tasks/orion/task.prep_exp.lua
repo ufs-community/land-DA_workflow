fa093597913f0b5820903c092a160d62641e4127
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-03-22 17:57:36 -0500

    fix module issues

--- modulefiles/tasks/orion/task.prep_exp.lua
+++ modulefiles/tasks/orion/task.prep_exp.lua
@@ -1,1 +1,8 @@
+prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
+prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack_jedi"))
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+load(pathJoin("stack-python", stack_python_ver))
+
 load(pathJoin("prod-util", prod_util_ver))
--- parm/task_load_modules_run_jjob.sh
+++ parm/task_load_modules_run_jjob.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 4 ]; then
   echo "Incorrect number of arguments specified:
   Number of arguments specified:  $#
 
-Usage: task_load_modules.sh task_name  jjob_fp
+Usage: task_load_modules_run_jjob.sh task_name home_dir machine_name jjob_fn
 
 where the arguments are defined as follows:
   task_name:
@@ -39,7 +39,7 @@ else
   echo "FATAL ERROR: version file does not exist !!!"
 fi
 module_dp="${home_dir}/modulefiles/tasks/${machine}"
-modue use "${module_dp}"
+module use "${module_dp}"
 
 # Load module file for a specific task
 task_module_fn="task.${task_name}"
@@ -49,3 +49,6 @@ if [ -f "${module_dp}/${task_module_fn}.lua" ]; then
 else
   echo "FATAL ERROR: task module file does not exist !!!"
 fi
+
+# Run J-job script
+${home_dir}/jobs/${jjob_fn}
