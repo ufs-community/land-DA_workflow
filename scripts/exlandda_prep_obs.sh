2acae8765020daf4733da8bf1466c1dd68c44b80
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-04-26 10:02:29 -0500

    change indentation

--- scripts/exlandda_prep_obs.sh
+++ scripts/exlandda_prep_obs.sh
@@ -37,28 +37,27 @@ OBSDIR=${LANDDA_INPUTS}/DA
 # 2. PREPARE OBS FILES
 ################################################
 for obs in "${OBS_TYPES[@]}"; do
-    # get the. obs file name
-    if [ ${obs} == "GTS" ]; then
-      obsfile=$OBSDIR/snow_depth/GTS/data_proc/${YYYY}${MM}/adpsfc_snow_${YYYY}${MM}${DD}${HH}.nc4
-      # GHCN are time-stamped at 18. If assimilating at 00, need to use previous day's obs, so that
-      # obs are within DA window.
-    elif [ $ATMOS_FORC == "era5" ] && [ ${obs} == "GHCN" ]; then
-      obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/ghcn_snwd_ioda_${YYYP}${MP}${DP}.nc
-    elif [ $ATMOS_FORC == "gswp3" ] && [ ${obs} == "GHCN" ]; then
-      obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/fake_ghcn_snwd_ioda_${YYYP}${MP}${DP}_jediv7.nc
-    elif [ ${obs} == "SYNTH" ]; then
-      obsfile=$OBSDIR/synthetic_noahmp/IODA.synthetic_gswp_obs.${YYYY}${MM}${DD}${HH}.nc
-    else
-      echo "do_landDA: Unknown obs type requested ${obs}, exiting"
-      exit 1
-    fi
+  # get the. obs file name
+  if [ ${obs} == "GTS" ]; then
+    obsfile=$OBSDIR/snow_depth/GTS/data_proc/${YYYY}${MM}/adpsfc_snow_${YYYY}${MM}${DD}${HH}.nc4
+    # GHCN are time-stamped at 18. If assimilating at 00, need to use previous day's obs, so that
+    # obs are within DA window.
+  elif [ $ATMOS_FORC == "era5" ] && [ ${obs} == "GHCN" ]; then
+    obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/ghcn_snwd_ioda_${YYYP}${MP}${DP}.nc
+  elif [ $ATMOS_FORC == "gswp3" ] && [ ${obs} == "GHCN" ]; then
+    obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/fake_ghcn_snwd_ioda_${YYYP}${MP}${DP}_jediv7.nc
+  elif [ ${obs} == "SYNTH" ]; then
+    obsfile=$OBSDIR/synthetic_noahmp/IODA.synthetic_gswp_obs.${YYYY}${MM}${DD}${HH}.nc
+  else
+    echo "do_landDA: Unknown obs type requested ${obs}, exiting"
+    exit 1
+  fi
 
-    # check obs are available
-    if [[ -e $obsfile ]]; then
-	echo "do_landDA: $i observations found: $obsfile"
-	ln -fs $obsfile  ${obs}_${YYYY}${MM}${DD}${HH}.nc
-    else
-	echo "${obs} observations not found: $obsfile"
-	#    JEDI_TYPES[$ii]="SKIP"
-    fi
+  # check obs are available
+  if [[ -e $obsfile ]]; then
+    echo "do_landDA: $i observations found: $obsfile"
+    ln -fs $obsfile  ${obs}_${YYYY}${MM}${DD}${HH}.nc
+  else
+    echo "${obs} observations not found: $obsfile"
+  fi
 done
