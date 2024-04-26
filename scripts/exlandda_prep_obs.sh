9f0d9fce1c18c12e6230c2ad58e2069170bfda8a
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-04-26 09:59:17 -0500

    change path to input data dir for fake files

--- scripts/exlandda_prep_obs.sh
+++ scripts/exlandda_prep_obs.sh
@@ -39,20 +39,18 @@ OBSDIR=${LANDDA_INPUTS}/DA
 for obs in "${OBS_TYPES[@]}"; do
     # get the. obs file name
     if [ ${obs} == "GTS" ]; then
-	obsfile=$OBSDIR/snow_depth/GTS/data_proc/${YYYY}${MM}/adpsfc_snow_${YYYY}${MM}${DD}${HH}.nc4
-	# GHCN are time-stamped at 18. If assimilating at 00, need to use previous day's obs, so that
-	# obs are within DA window.
+      obsfile=$OBSDIR/snow_depth/GTS/data_proc/${YYYY}${MM}/adpsfc_snow_${YYYY}${MM}${DD}${HH}.nc4
+      # GHCN are time-stamped at 18. If assimilating at 00, need to use previous day's obs, so that
+      # obs are within DA window.
     elif [ $ATMOS_FORC == "era5" ] && [ ${obs} == "GHCN" ]; then
-	obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/ghcn_snwd_ioda_${YYYP}${MP}${DP}.nc
+      obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/ghcn_snwd_ioda_${YYYP}${MP}${DP}.nc
     elif [ $ATMOS_FORC == "gswp3" ] && [ ${obs} == "GHCN" ]; then
-#	obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/fake_ghcn_snwd_ioda_${YYYP}${MP}${DP}.nc
-        obsfile=/work/noaa/epic/chjeon/nc_var_change/data/fake_ghcn_snwd_ioda_${YYYP}${MP}${DP}_new.nc
-#        obsfile=/work/noaa/epic/chjeon/nc_var_change/data/ghcn_snwd_ioda_20230501.nc
+      obsfile=$OBSDIR/snow_depth/GHCN/data_proc/v3/${YYYY}/fake_ghcn_snwd_ioda_${YYYP}${MP}${DP}_jediv7.nc
     elif [ ${obs} == "SYNTH" ]; then
-	obsfile=$OBSDIR/synthetic_noahmp/IODA.synthetic_gswp_obs.${YYYY}${MM}${DD}${HH}.nc
+      obsfile=$OBSDIR/synthetic_noahmp/IODA.synthetic_gswp_obs.${YYYY}${MM}${DD}${HH}.nc
     else
-	echo "do_landDA: Unknown obs type requested ${obs}, exiting"
-	exit 1
+      echo "do_landDA: Unknown obs type requested ${obs}, exiting"
+      exit 1
     fi
 
     # check obs are available
