3f79d9116de49ab4e1a454d9c7cf97e818472bb2
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-06-03 20:02:11 -0500

    simplify python script

--- scripts/exlandda_plot_stats.sh
+++ scripts/exlandda_plot_stats.sh
@@ -14,19 +14,19 @@ INPUTFP="${DATA_HOFX}"
 FIELDVAR="OMA"
 #
 NBINS=100
-# Plot type
-PLOTTYPE="scatter"
+# Plot type (scatter/histogram/both)
+PLOTTYPE="both"
 # Figure title
 FIGTITLE="GHCN Snow Depth (mm)::Obs-Ana::${PDY}"
-# Output file name
-OUTPUTFN="hofx_oma_${PLOTTYPE}_${PDY}.png"
+# Prefix of output file name
+PREOUTFN="hofx_oma_${PDY}"
 
 sed -i "s|INPUTFP|${INPUTFP}|g" plot_hofx.yaml
 sed -i -e "s/XXFIELDVAR/${FIELDVAR}/g" plot_hofx.yaml
 sed -i -e "s/XXNBINS/${NBINS}/g" plot_hofx.yaml
 sed -i -e "s/XXPLOTTYPE/${PLOTTYPE}/g" plot_hofx.yaml
 sed -i -e "s/XXFIGTITLE/${FIGTITLE}/g" plot_hofx.yaml
-sed -i -e "s/XXOUTPUTFN/${OUTPUTFN}/g" plot_hofx.yaml
+sed -i -e "s/XXPREOUTFN/${PREOUTFN}/g" plot_hofx.yaml
 
 ${USHlandda}/hofx_analysis_stats.py
 if [[ $? != 0 ]]; then
@@ -34,38 +34,5 @@ if [[ $? != 0 ]]; then
   exit 33
 fi
 
-############################################################
-# Histogram Plot
-############################################################
-
-cp ${PARMlandda}/templates/template.plot_hofx.yaml plot_hofx.yaml
-
-# Path to the directory containing the input file
-INPUTFP="${DATA_HOFX}"
-# Field variable
-FIELDVAR="OMA"
-#
-NBINS=100
-# Plot type
-PLOTTYPE="histogram"
-# Figure title
-FIGTITLE="GHCN Snow Depth (mm)::Obs-Ana::${PDY}"
-# Output file name
-OUTPUTFN="hofx_oma_${PLOTTYPE}_${PDY}.png"
-
-sed -i "s|INPUTFP|${INPUTFP}|g" plot_hofx.yaml
-sed -i -e "s/XXFIELDVAR/${FIELDVAR}/g" plot_hofx.yaml
-sed -i -e "s/XXNBINS/${NBINS}/g" plot_hofx.yaml
-sed -i -e "s/XXPLOTTYPE/${PLOTTYPE}/g" plot_hofx.yaml
-sed -i -e "s/XXFIGTITLE/${FIGTITLE}/g" plot_hofx.yaml
-sed -i -e "s/XXOUTPUTFN/${OUTPUTFN}/g" plot_hofx.yaml
-
-${USHlandda}/hofx_analysis_stats.py
-if [[ $? != 0 ]]; then
-  echo "Histogram Plot failed"
-  exit 34
-fi
-
-
 # Copy result files to COMOUT
-cp -p ${OUTPUTFN} ${COMOUTplot}
+cp -p ${PREOUTFN}* ${COMOUTplot}
--- ush/hofx_analysis_stats.py
+++ ush/hofx_analysis_stats.py
@@ -13,37 +13,37 @@ import matplotlib.ticker
 
 def get_obs_stats(fdir, plottype):
     global lat,lon
-    omb_= []
-    oma_= []    
-    obs_= []
-    qc_ = []
-    err_= []
-    lat_= []
-    lon_= []
+    omb_ = []
+    oma_ = []    
+    obs_ = []
+    qc_  = []
+    err_ = []
+    lat_ = []
+    lon_ = []
     
     for fname in os.listdir(fdir):
-        print("FN:",fname)
-        f= netCDF4.Dataset(fdir+'/'+fname)
+        print("=== File Name:",fname)
+        f = netCDF4.Dataset(fdir+'/'+fname)
 #        print("NETCDF:",f)
-        obs= f.groups['ObsValue'].variables['totalSnowDepth']
+        obs = f.groups['ObsValue'].variables['totalSnowDepth']
 #        print("ObsValue:",obs)
-        ombg= f.groups['ombg'].variables['totalSnowDepth']
+        ombg = f.groups['ombg'].variables['totalSnowDepth']
 #        print("OMBG:",ombg)
-        oman= f.groups['oman'].variables['totalSnowDepth']
+        oman = f.groups['oman'].variables['totalSnowDepth']
 #        print("OMAN:",oman)
-        qc= f.groups['PreQC'].variables['totalSnowDepth']
+        qc = f.groups['PreQC'].variables['totalSnowDepth']
 #        print("PreQC:",qc)
-        obstime= f.groups['MetaData'].variables['dateTime']
+        obstime = f.groups['MetaData'].variables['dateTime']
 #        print("OBS_TIME:",obstime)
         if plottype == 'histogram':
-            ombg_= np.ma.masked_where(qc != 0, ombg)
-            ombg_= np.ma.masked_where(ombg == 0, ombg_) 
-            oman_= np.ma.masked_where(qc != 0, oman)
-            oman_= np.ma.masked_where(ombg == 0, oman_)
-            ombg= ombg_
-            oman= oman_
-        lat= f.groups['MetaData'].variables['latitude']
-        lon= f.groups['MetaData'].variables['longitude']
+            ombg_ = np.ma.masked_where(qc != 0, ombg)
+            ombg_ = np.ma.masked_where(ombg == 0, ombg_) 
+            oman_ = np.ma.masked_where(qc != 0, oman)
+            oman_ = np.ma.masked_where(ombg == 0, oman_)
+            ombg = ombg_
+            oman = oman_
+        lat = f.groups['MetaData'].variables['latitude']
+        lon = f.groups['MetaData'].variables['longitude']
 
         obs_.append(obs[:])
         omb_.append(ombg[:])
@@ -51,59 +51,61 @@ def get_obs_stats(fdir, plottype):
         lat_.append(lat[:])
         lon_.append(lon[:])
 
-    total_omb= np.concatenate(omb_)
-    total_oma= np.concatenate(oma_)
-    total_obs= np.concatenate(obs_)
-    total_lat= np.concatenate(lat_)
-    total_lon= np.concatenate(lon_)
+    total_omb = np.concatenate(omb_)
+    total_oma = np.concatenate(oma_)
+    total_obs = np.concatenate(obs_)
+    total_lat = np.concatenate(lat_)
+    total_lon = np.concatenate(lon_)
 
     return total_oma,total_omb,total_lat,total_lon
 
 def plot_scatter():
-    field_mean= float("{:.2f}".format(np.mean(np.absolute(field))))
-    field_std= float("{:.2f}".format(np.std(np.absolute(field))))
-
-    crs= ccrs.PlateCarree()
-    fig= plt.figure(figsize=(8,5))
-    ax= plt.subplot(111, projection=crs)
+    print("===== PLOT: SCATTER =====")
+    field_mean = float("{:.2f}".format(np.mean(np.absolute(field))))
+    field_std = float("{:.2f}".format(np.std(np.absolute(field))))
+    crs = ccrs.PlateCarree()
+    fig = plt.figure(figsize=(8,5))
+    ax = plt.subplot(111, projection=crs)
     ax.coastlines(resolution='110m')
-    colors= ['red','red','red','blue','red','blue']
-    norm= plt.Normalize(yaml_data['field_range'][0],yaml_data['field_range'][1])
-    sc= ax.scatter(lon, lat, c=field, s=2.0, cmap='bwr', transform=crs, norm=norm)
-    cbar= plt.colorbar(sc, orientation="horizontal", shrink=0.5, pad=0.05)
-    stitle= yaml_data['title_fig']+' \n '+'Mean |OMA| ='+str(field_mean)+', STDV |OMA| ='+str(field_std)
+    colors = ['red','red','red','blue','red','blue']
+    norm = plt.Normalize(yaml_data['field_range'][0],yaml_data['field_range'][1])
+    sc = ax.scatter(lon, lat, c=field, s=2.0, cmap='bwr', transform=crs, norm=norm)
+    cbar = plt.colorbar(sc, orientation="horizontal", shrink=0.5, pad=0.05)
+    stitle = yaml_data['title_fig']+' \n '+'Mean |OMA| ='+str(field_mean)+', STDV |OMA| ='+str(field_std)
     plt.title(stitle)
-    plt.savefig(yaml_data['outputfig'])
-    plt.show()
+    output_fn = yaml_data['output_prefix']+"_scatter.png"
+    plt.savefig(output_fn,dpi=300,bbox_inches='tight')
+    plt.close('all')
 
 def plot_histogram():
-    field_mean= float("{:.2f}".format(np.mean(field)))
-    field_std= float("{:.2f}".format(np.std(field)))
-
-    nbins= yaml_data['nbins']
-    xlimit= yaml_data['field_range']
+    print("===== PLOT: HISTOGRAM =====")    
+    field_mean = float("{:.2f}".format(np.mean(field)))
+    field_std = float("{:.2f}".format(np.std(field)))
+    nbins = yaml_data['nbins']
+    xlimit = yaml_data['field_range']
     plt.hist(field[:], bins=nbins, range=xlimit, density=True, color ="blue")
-    stitle= yaml_data['title_fig']+' \n '+'Mean(OMA) ='+str(field_mean)+', STDV(OMA) ='+str(field_std)
+    stitle = yaml_data['title_fig']+' \n '+'Mean(OMA) ='+str(field_mean)+', STDV(OMA) ='+str(field_std)
     plt.title(stitle)
-    plt.savefig(yaml_data['outputfig'])
-    plt.show()
+    output_fn = yaml_data['output_prefix']+"_histogram.png"
+    plt.savefig(output_fn,dpi=300,bbox_inches='tight')
+    plt.close('all')
 
 if __name__ == '__main__':
     global field,yaml_data
 
-    yaml_file= "plot_hofx.yaml"
+    yaml_file = "plot_hofx.yaml"
     with open(yaml_file, 'r') as f:
-        yaml_data= yaml.load(f, Loader=yaml.FullLoader)
+        yaml_data = yaml.load(f, Loader=yaml.FullLoader)
     f.close()
     print("YAML_DATA:",yaml_data)
     oma,omb,lat,lon= get_obs_stats(yaml_data['hofx_files'],yaml_data['plottype'])
     
     if yaml_data['field_var'] == 'OMA':
-        field= oma
+        field = oma
     if yaml_data['field_var'] == 'OMB':
-        field= omb    
+        field = omb    
 
-    if yaml_data['plottype'] == 'scatter': 
+    if yaml_data['plottype'] == 'scatter' or yaml_data['plottype'] == 'both': 
         plot_scatter()
-    if yaml_data['plottype'] == 'histogram':
+    if yaml_data['plottype'] == 'histogram' or yaml_data['plottype'] == 'both':
         plot_histogram()
