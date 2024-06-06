6cf2fc5357d1fc166fafa4d848daf21bc382b8b6
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2024-06-06 19:36:34 +0000

    add rocoto launch script

---
+++ parm/launch_rocoto_wflow.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# Set shell options.
+set -xue
+
+# Set path
+PARMdir=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+source ${PARMdir}/detect_platform.sh
+
+# Load rocoto
+if [ "${MACHINE}" == "orion" ]; then
+  module load contrib
+  module load rocoto
+else
+  module load rocoto
+fi
+
+# Set file names.
+WFLOW_XML_FN="land_analysis.xml"
+rocoto_xml_bn=$( basename "${WFLOW_XML_FN}" ".xml" )
+rocoto_database_fn="${rocoto_xml_bn}.db"
+WFLOW_LOG_FN="log.rocoto_launch"
+
+# Initialize the default status of the workflow to "IN PROGRESS".
+wflow_status="IN PROGRESS"
+
+# crontab line
+CRONTAB_LINE=""
+
+cd "${PARMdir}"
+rocotorun_cmd="rocotorun -w \"${WFLOW_XML_FN}\" -d \"${rocoto_database_fn}\""
+eval ${rocotorun_cmd}
+
+rocotostat_output=$( rocotostat -w ${WFLOW_XML_FN} -d ${rocoto_database_fn} )
+
+error_msg="DEAD"
+while read -r line; do
+  if [ echo "$line" | grep -q "${error_msg}" ]; then
+    wflow_status="FAILURE"
+    break
+  fi
+done <<< ${rocotostat_output}
+
+# Print out rocotostat
+printf "%s" "
+${rocotostat_output}
+" > "${WFLOW_LOG_FN}"
+
+# rocotostat with -s for cycle info
+rocotostat_s_output=$( rocotostat -w ${WFLOW_XML_FN} -d ${rocoto_database_fn} -s )
+
+regex_search="^[ ]*([0-9]+)[ ]+([A-Za-z]+)[ ]+.*"
+cycle_str=()
+cycle_status=()
+i=0
+while read -r line; do
+  if [ $i -gt 0 ]; then
+    im1=$((i-1))
+    cycle_str[im1]=$( echo "$line" | sed -r -n -e "s/${regex_search}/\1/p" )
+    cycle_status[im1]=$( echo "$line" | sed -r -n -e "s/${regex_search}/\2/p" )
+  fi
+  i=$((i+1))
+done <<< "${rocotostat_s_output}"
+
+# Get the number of cycles
+num_cycles_total=${#cycle_str[@]}
+num_cycles_completed=0
+for (( i=0; i<=$((num_cycles_total-1)); i++ )); do
+  if [ "${cycle_status[i]}" = "Done" ]; then
+    num_cycles_completed=$((num_cycles_completed+1))
+  fi
+done
+
+# Check whether all cycles are complete
+if [ ${num_cycles_completed} -eq ${num_cycles_total} ]; then
+  wflow_status="SUCCESS"
+fi
+
+# Print out result
+printf "%s" "
+
+Summary of workflow status:
+=====================================================
+  ${num_cycles_completed} out of ${num_cycles_total} cycles completed.
+  Workflow status:  ${wflow_status}
+=====================================================
+" >> ${WFLOW_LOG_FN}
+
+# Remove crontab line
+if [ "${wflow_status}" = "SUCCESS" ] || [ "${wflow_status}" = "FAILURE" ]; then
+  msg="The crontab line is removed:
+  CRONTAB_LINE = \"${CRONTAB_LINE}\" "
+
+  $USHdir/get_crontab_contents.py --remove -m=${MACHINE} -l="${CRONTAB_LINE}" -c -d
+
+  printf "%s" "$msg" >> ${WFLOW_LOG_FN}
+fi
