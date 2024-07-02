bbbcf603174215c6d5d22d1de1fc89e1224ab269
Chan-Hoo Jeon <chan-hoo.jeon@noaa.gov>
2024-07-02 13:44:46 -0500

    change time limit

--- sorc/test/run_orion_ctest.sh
+++ sorc/test/run_orion_ctest.sh
@@ -5,7 +5,7 @@ JOB_ID=$(sbatch --job-name=ctest --account=epic --qos=batch --ntasks-per-node=7 
 
 CHECK_ID=$(sbatch --job-name=ctest --account=epic --qos=debug --ntasks-per-node=1 --nodes=1 --time=00:01:00 --dependency=afterok:$JOB_ID ./check_ctest.sh)
 
-sleep 3m
+sleep 30m
 
 if [ -f out.ctest ]; then
     cat out.ctest
