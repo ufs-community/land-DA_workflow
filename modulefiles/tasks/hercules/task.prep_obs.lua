75f045cda47e3d873ae193a72f2bd68dd177410b
chan-hoo <chan-hoo.jeon@noaa.gov>
2024-08-09 10:17:25 -0500

    add yaml file

---
+++ modulefiles/tasks/hercules/task.prep_obs.lua
@@ -0,0 +1,7 @@
+prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
+
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
+load(pathJoin("stack-python", stack_python_ver))
+
+load(pathJoin("prod_util", prod_util_ver))
