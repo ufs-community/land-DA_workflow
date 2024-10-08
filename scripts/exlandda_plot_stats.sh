96de8e0a30b8766da75c4122b28d3a2f328be529
Chan-Hoo.Jeon-NOAA <60152248+chan-hoo@users.noreply.github.com>
2024-10-08 12:13:05 -0400

    Add a plot option for restart files of forecast task (#149)
    * change order of plot_stat
    
    * add plot for forecast restart
    
    * fix script failure

--- scripts/exlandda_plot_stats.sh
+++ scripts/exlandda_plot_stats.sh
@@ -2,11 +2,16 @@
 
 set -xue
 
+YYYY=${PDY:0:4}
+MM=${PDY:4:2}
+DD=${PDY:6:2}
+HH=${cyc}
+
 ############################################################
-# Scatter Plot
+# Stats Plot
 ############################################################
 
-cp ${PARMlandda}/templates/template.plot_hofx.yaml plot_hofx.yaml
+cp -p ${PARMlandda}/templates/template.plot_hofx.yaml plot_hofx.yaml
 
 # Path to the directory containing the input file
 INPUTFP="${DATA_HOFX}"
@@ -36,9 +41,48 @@ sed -i -e "s/XXMACHINE/${MACHINE}/g" plot_hofx.yaml
 
 ${USHlandda}/hofx_analysis_stats.py
 if [[ $? != 0 ]]; then
-  echo "Scatter/Histogram plots failed"
+  echo "FATAL ERROR: Scatter/Histogram plots failed"
   exit 33
 fi
 
 # Copy result files to COMOUT
 cp -p ${PREOUTFN}* ${COMOUTplot}
+
+
+############################################################
+# Restart Plot
+############################################################
+
+nYYYY=${NTIME:0:4}
+nMM=${NTIME:4:2}
+nDD=${NTIME:6:2}
+nHH=${NTIME:8:2}
+
+cp -p ${PARMlandda}/templates/template.plot_restart.yaml plot_restart.yaml
+
+PATH_DATA="${COMIN}"
+WORK_DIR="${DATA}"
+FN_DATA_BASE="ufs_land_restart.${nYYYY}-${nMM}-${nDD}_${nHH}-00-00.tile"
+FN_DATA_EXT=".nc"
+SOIL_LEVEL_NUMBER="1"
+OUT_TITLE_BASE="Land-DA::restart::${nYYYY}-${nMM}-${nDD}_${nHH}::"
+OUT_FN_BASE="landda_out_restart_${nYYYY}-${nMM}-${nDD}_${nHH}_"
+
+sed -i "s|PATH_DATA|${PATH_DATA}|g" plot_restart.yaml
+sed -i "s|WORK_DIR|${WORK_DIR}|g" plot_restart.yaml
+sed -i -e "s/XXFN_DATA_BASE/${FN_DATA_BASE}/g" plot_restart.yaml
+sed -i -e "s/XXFN_DATA_EXT/${FN_DATA_EXT}/g" plot_restart.yaml
+sed -i -e "s/XXSOIL_LEVEL_NUMBER/${SOIL_LEVEL_NUMBER}/g" plot_restart.yaml
+sed -i -e "s/XXOUT_TITLE_BASE/${OUT_TITLE_BASE}/g" plot_restart.yaml
+sed -i -e "s/XXOUT_FN_BASE/${OUT_FN_BASE}/g" plot_restart.yaml
+sed -i -e "s/XXMACHINE/${MACHINE}/g" plot_restart.yaml
+
+${USHlandda}/plot_forecast_restart.py
+if [[ $? != 0 ]]; then
+  echo "FATAL ERROR: Forecast restart plots failed"
+  exit 44
+fi
+
+# Copy result files to COMOUT
+cp -p ${OUT_FN_BASE}* ${COMOUTplot}
+
