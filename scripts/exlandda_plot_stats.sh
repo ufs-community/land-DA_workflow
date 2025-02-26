04edf04fcdb7b39c62c0979bce0e0c7d2c984724
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2025-02-26 20:15:41 +0000

    turn all plots on

--- scripts/exlandda_plot_stats.sh
+++ scripts/exlandda_plot_stats.sh
@@ -15,10 +15,10 @@ nMM=${NTIME:4:2}
 nDD=${NTIME:6:2}
 nHH=${NTIME:8:2}
 
-DO_PLOT_STATS="NO"
+DO_PLOT_STATS="YES"
 DO_PLOT_TIME_HISTORY="YES"
-DO_PLOT_RESTART="NO"
-DO_PLOT_COMBINE_TILES="NO"
+DO_PLOT_RESTART="YES"
+DO_PLOT_COMBINE_TILES="YES"
 
 ############################################################
 # Stats Plot
