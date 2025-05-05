a93cf2f1cc21f80f0ff7fbf5f83cecd885696737
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2025-05-05 19:09:08 +0000

    add variable and paths

--- modulefiles/tasks/singularity/task.pre_anal.lua
+++ modulefiles/tasks/singularity/task.pre_anal.lua
@@ -1,2 +1,4 @@
 load("COMPILER")
 load("MPI")
+prepend_path("PATH", "SINGULARITY_WORKING_DIR/land-DA_workflow/exec")
+prepend_path("PATH", "SINGULARITY_WORKING_DIR/land-DA_workflow/sorc/conda/envs/land_da/bin"
