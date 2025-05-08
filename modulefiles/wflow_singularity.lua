012eb0f21f061915db19f2a8b268f01af6dba2f3
EdwardSnyder-NOAA <96196752+EdwardSnyder-NOAA@users.noreply.github.com>
2025-05-08 05:43:11 -0500

    [develop] Changes scripts for Container (#228)
    * init commit
    
    * add variable and paths
    
    * expanded singularity support
    
    * update location to fix directory
    
    * fix typo
    
    * changes for da workshop
    
    * expand to singularity coverage

---
+++ modulefiles/wflow_singularity.lua
@@ -0,0 +1,10 @@
+help([[
+This module loads python environement for running the land-DA workflow with
+Singularity container
+]])
+
+whatis([===[Loads libraries needed for running the land-DA workflow with Singularity container ]===])
+
+load("rocoto")
+
+prepend_path("PATH", "SINGULARITY_WORKING_DIR/land-DA_workflow/sorc/conda/envs/land_da/bin")
