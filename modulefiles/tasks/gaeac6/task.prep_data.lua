7647fe936c98a275a5d8fefb38ebd25ef7557278
Chan-hoo Jeon <Chan-hoo.Jeon@gaea67.ncrc.gov>
2025-07-16 15:42:56 -0400

    fix modulefile for gaea-c6

--- modulefiles/tasks/gaeac6/task.prep_data.lua
+++ modulefiles/tasks/gaeac6/task.prep_data.lua
@@ -1,4 +1,4 @@
-prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack_unienv"))
+prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
