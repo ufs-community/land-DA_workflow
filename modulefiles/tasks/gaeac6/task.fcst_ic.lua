0923f5c3291929968a28ba1321c7f820ca89793f
Chan-hoo Jeon <Chan-hoo.Jeon@gaea64.ncrc.gov>
2025-07-23 16:41:34 -0400

    fix module files for gaeac6

--- modulefiles/tasks/gaeac6/task.fcst_ic.lua
+++ modulefiles/tasks/gaeac6/task.fcst_ic.lua
@@ -2,8 +2,6 @@ prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
 
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
-load(pathJoin("stack-python", stack_python_ver))
-
 load(pathJoin("jasper", jasper_ver))
 load(pathJoin("hdf5", hdf5_ver))
 load(pathJoin("netcdf-c", netcdf_c_ver))
--- modulefiles/tasks/gaeac6/task.forecast.lua
+++ modulefiles/tasks/gaeac6/task.forecast.lua
@@ -3,7 +3,6 @@ prepend_path("MODULEPATH", os.getenv("modulepath_spack_stack"))
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 load(pathJoin("stack-python", stack_python_ver))
-
 load(pathJoin("jasper", jasper_ver))
 load(pathJoin("zlib", zlib_ver))
 load(pathJoin("libpng", libpng_ver))
@@ -24,6 +23,8 @@ load(pathJoin("gftl-shared", gftl_shared_ver))
 load(pathJoin("mapl", mapl_ver))
 load(pathJoin("prod_util", prod_util_ver))
 
-prepend_path("MODULEPATH", os.getenv("modulepath_pymodule"))
+load(pathJoin("py-jinja2", py_jinja2_ver))
+load(pathJoin("py-netcdf4", py_netcdf4_ver))
+load(pathJoin("py-numpy", py_numpy_ver))
+load(pathJoin("py-pyyaml", py_pyyaml_ver))
 
-load("python-ufs-land-da-wflow")
