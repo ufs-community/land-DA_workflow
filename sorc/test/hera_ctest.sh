1b73a6dca3ff85d98322ea677aa123b8ace93c65
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2025-08-04 13:36:11 +0000

    change path to jedi for ctest on hera

--- sorc/test/hera_ctest.sh
+++ sorc/test/hera_ctest.sh
@@ -8,8 +8,11 @@ source ../../versions/build.ver_hera
 module use ../../modulefiles
 module load build_hera_intel
 
+module use /scratch3/NAGAPE/epic/ufs-conda/modulefiles
+module load python-ufs-land-da-wflow
+
 export MPIRUN="srun"
-export JEDI_PATH="/scratch2/NAGAPE/epic/UFS_Land-DA_v2.1/jedi_bundle_sync"
+export JEDI_PATH="/scratch3/NAGAPE/epic/UFS_Land-DA_v2.1/jedi_bundle_hera"
 
 ctest
 
