7014488ef12849616c13a65d4974141dbe25ac58
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2025-08-04 13:17:26 +0000

    update path to v3.0 for other machines

--- parm/setup_wflow_env.py
+++ parm/setup_wflow_env.py
@@ -331,23 +331,23 @@ def set_machine_parm(machine):
             MAX_CORES_PER_NODE = 192
             WARMSTART_DIR = "/gpfs/f6/bil-fire8/world-shared/UFS_Land-DA_v3.0/inputs/DATA_RESTART"
         case "hera":
-            CUSTOM_JEDI_CONFIG_PATH = "/scratch2/NAGAPE/epic/UFS_Land-DA_v2.1/inputs/test_base/jedi_yaml"
-            JEDI_IODACONV_PATH = "/scratch2/NAGAPE/epic/UFS_Land-DA_v2.1/jedi_bundle_sync/build/lib/python3.11"
-            JEDI_PATH = "/scratch2/NAGAPE/epic/UFS_Land-DA_v2.1/jedi_bundle_sync"
+            CUSTOM_JEDI_CONFIG_PATH = "/scratch3/NAGAPE/epic/UFS_Land-DA_v3.0/inputs/test_base/jedi_yaml"
+            JEDI_IODACONV_PATH = "/scratch3/NAGAPE/epic/UFS_Land-DA_v3.0/jedi_bundle_hera/build/lib/python3.11"
+            JEDI_PATH = "/scratch3/NAGAPE/epic/UFS_Land-DA_v3.0/jedi_bundle_hera"
             MAX_CORES_PER_NODE = 40
-            WARMSTART_DIR = "/scratch2/NAGAPE/epic/UFS_Land-DA_v2.1/inputs/DATA_RESTART"
+            WARMSTART_DIR = "/scratch3/NAGAPE/epic/UFS_Land-DA_v3.0/inputs/DATA_RESTART"
         case "hercules":
-            CUSTOM_JEDI_CONFIG_PATH = "/work/noaa/epic/UFS_Land-DA_v2.1/inputs/test_base/jedi_yaml"
+            CUSTOM_JEDI_CONFIG_PATH = "/work/noaa/epic/UFS_Land-DA_v3.0/inputs/test_base/jedi_yaml"
             JEDI_IODACONV_PATH = "/work/noaa/epic/UFS_Land-DA_v3.0/jedi_bundle_hercules/build/lib/python3.11"
             JEDI_PATH = "/work/noaa/epic/UFS_Land-DA_v2.1/jedi_bundle_hercules"
             MAX_CORES_PER_NODE = 80
-            WARMSTART_DIR = "/work/noaa/epic/UFS_Land-DA_v2.1/inputs/DATA_RESTART"
+            WARMSTART_DIR = "/work/noaa/epic/UFS_Land-DA_v3.0/inputs/DATA_RESTART"
         case "orion":
-            CUSTOM_JEDI_CONFIG_PATH = "/work/noaa/epic/UFS_Land-DA_v2.1/inputs/test_base/jedi_yaml"
-            JEDI_IODACONV_PATH = "/work/noaa/epic/UFS_Land-DA_v2.1/jedi_bundle_orion/build/lib/python3.11"
+            CUSTOM_JEDI_CONFIG_PATH = "/work/noaa/epic/UFS_Land-DA_v3.0/inputs/test_base/jedi_yaml"
+            JEDI_IODACONV_PATH = "/work/noaa/epic/UFS_Land-DA_v3.0/jedi_bundle_orion/build/lib/python3.11"
             JEDI_PATH = "/work/noaa/epic/UFS_Land-DA_v2.1/jedi_bundle_orion"
             MAX_CORES_PER_NODE = 40
-            WARMSTART_DIR = "/work/noaa/epic/UFS_Land-DA_v2.1/inputs/DATA_RESTART"
+            WARMSTART_DIR = "/work/noaa/epic/UFS_Land-DA_v3.0/inputs/DATA_RESTART"
         case "singularity":
             CUSTOM_JEDI_CONFIG_PATH = "SINGULARITY_WORKING_DIR"
             JEDI_IODACONV_PATH = "SINGULARITY_WORKING_DIR"
