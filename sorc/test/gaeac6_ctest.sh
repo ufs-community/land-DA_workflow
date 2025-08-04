658412804003ff817c240dbb904607a9d87847df
Chan-hoo Jeon <Chan-hoo.Jeon@gaea64.ncrc.gov>
2025-08-04 07:02:07 -0400

    update ctest script for gaeac6

--- sorc/test/gaeac6_ctest.sh
+++ sorc/test/gaeac6_ctest.sh
@@ -3,10 +3,13 @@
 
 set -eux
 
-module purge
+module reset
 source ../../versions/build.ver_gaeac6
 module use ../../modulefiles
 module load build_gaeac6_intel
+
+module use /gpfs/f6/bil-fire8/world-shared/ufs-conda/modulefiles
+module load python-ufs-land-da-wflow
 
 export MPIRUN="srun"
 export JEDI_PATH="/gpfs/f6/bil-fire8/world-shared/UFS_Land-DA_v2.1/jedi_bundle_sync"
