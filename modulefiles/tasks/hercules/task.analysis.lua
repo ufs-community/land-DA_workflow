391b7028deb04a60d86ec510cc9e1f5eb1884e7f
chan-hoo <chan-hoo.jeon@noaa.gov>
2025-10-16 11:40:18 -0500

    fix missing lib issue in anaysis on hercuels

--- modulefiles/tasks/hercules/task.analysis.lua
+++ modulefiles/tasks/hercules/task.analysis.lua
@@ -1,1 +1,12 @@
-../common/task.analysis.lua
+prepend_path("MODULEPATH", os.getenv("modulepath_modulefiles"))
+
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_intel_oneapi_mpi_ver))
+load(pathJoin("intel-oneapi-mkl", intel_oneapi_mkl_ver))
+
+load(pathJoin("esmf", esmf_ver))
+load(pathJoin("prod_util", prod_util_ver))
+
+prepend_path("MODULEPATH", os.getenv("modulepath_pymodule"))
+
+load("python-ufs-land-da-wflow")
