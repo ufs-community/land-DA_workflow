9065f640ea6d07a9d53106cadb3f51e736501dd3
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2026-03-11 00:22:16 +0000

    switch what was loaded to prevent the conda mpiexec to run

--- modulefiles/tasks/singularity/task.jcb.lua
+++ modulefiles/tasks/singularity/task.jcb.lua
@@ -1,5 +1,5 @@
-load("COMPILER")
-load("MPI")
 prepend_path("PATH", "SINGULARITY_WORKING_DIR/land-DA_workflow/exec")
 prepend_path("PATH", "SINGULARITY_WORKING_DIR/land-DA_workflow/sorc/conda/envs/python-ufs-land-da-wflow/bin")
 setenv("NDATE", "SINGULARITY_WORKING_DIR/land-DA_workflow/exec/ndate")
+load("COMPILER")
+load("MPI")
